094788ee90aa8ae6cf0b9c0f03fc78d9237e4893
Yagnya <yagnyapatel@gmail.com>
2024-12-04 02:04:39 -0800

    Add walkthrough comments to agent.py

--- agent_service/agent.py
+++ agent_service/agent.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import AIMessage, ToolMessage, BaseMessage
 
+# [1] Wildcard companiion packages are imported here. Since we're using langgraph, we need to import the langgraph package
 from wildcard_core.tool_registry.tools.rest_api.types import ApiKeyAuthConfig, BearerAuthConfig, AuthType
 from wildcard_core.tool_search.utils.api_service import APIService
 from wildcard_langgraph import create_tool_selection_agent
@@ -17,22 +18,23 @@ from wildcard_core import ToolSearchClient
 def get_agent():
     load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
     
-    # Create the ToolSearchClient
+    # [2] The ToolSearchClient is used to search for tools that the agent can use. Let's create one here.
     tool_search_client = ToolSearchClient(api_key='alpha-api-access')
     
-    # Register necessary API authentications if you have them
+    # [3] Register necessary API authentications if you have them
     # tool_search_client.register_api_auth(
     #     APIService.NEW_YORK_TIMES,
     #     ApiKeyAuthConfig(type=AuthType.API_KEY, key_value=os.getenv("NYT_API_KEY"))
     # )
     
-    # Initialize the agent
+    # [4] Initialize the agent. We're using the ChatOpenAI model here.
     openai_api_key = os.getenv("OPENAI_API_KEY")
     model = ChatOpenAI(model="gpt-4o", temperature=0, api_key=openai_api_key)
 
-    # Tweak this prompt to change the behavior of the agent
+    # [5] Here's the fun part. Tweak this prompt to change the behavior of the agent
     task_system_prompt = """You are an autonomous personal assistant.
     """
 
+    # [6] Create the agent!
     agent, initial_state = create_tool_selection_agent(model, tool_search_client, task_system_prompt)
     return agent, initial_state, tool_search_client
